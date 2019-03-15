3bd23df05f20b7861a544cf52a23a0d2f31af3f3
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-03-15 03:39:35 -0400

    Update fixes

--- gitpush.ps1
+++ gitpush.ps1
@@ -1,4 +1,3 @@
-
 function Push-Master {
     param(
         [string]$GitRepoName,
@@ -6,15 +5,29 @@ function Push-Master {
         [string]$BranchName
     )
     Write-Information "Make sure you are firing this from right directory" -InformationAction Continue
-    git add .
-    git pull origin master
-    git commit -m $CommitMessage
-    git push origin $BranchName
+        git add .
+        git pull origin master
+        git commit -m $CommitMessage
+        git push origin $BranchName
 }
-Write-Information "Enter repo name" -InformationAction Continue
-$GitRepoName = Read-Host
-Write-Information "Enter Commit Message" -InformationAction Continue
-$CommitMessage = Read-Host
-Write-Information "Enter name of branch" -InformationAction Continue
-$BranchName = Read-Host
-Push-Master -GitRepoName $GitRepoName -CommitMessage $CommitMessage -BranchName $BranchName
+
+function Start{
+    param (
+        [string]$default 
+    )
+        if(($default -like "*yeah*") -or ($default -like "*yes*")){
+            Push-Master -GitRepoName "MKSAutoScripts" -CommitMessage "Update fixes" -BranchName "master"
+        }
+        else{
+                Write-Information "Enter repo name" -InformationAction Continue
+                $GitRepoName = Read-Host
+                Write-Information "Enter Commit Message" -InformationAction Continue
+                $CommitMessage = Read-Host
+                Write-Information "Enter name of branch" -InformationAction Continue
+                $BranchName = Read-Host
+                Push-Master -GitRepoName $GitRepoName -CommitMessage $CommitMessage -BranchName $BranchName
+            }
+}
+Write-Information "your legendary style" -InformationAction Continue
+    $default = Read-Host
+Start -default $default
